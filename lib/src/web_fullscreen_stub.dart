d532a16079072d09135a9a85e4a8b1936e081104
Ortes <malo.allee@gmail.com>
2026-09-07 20:04:18 +0200

    Revert "fix: feature-detect the Fullscreen API before calling it"
    This reverts commit c13cfd15d6b24149bb33d484587cb2c89c1174cb.

--- lib/src/web_fullscreen_stub.dart
+++ lib/src/web_fullscreen_stub.dart
@@ -1,6 +1,5 @@
 void requestBrowserFullscreen() {}
 void exitBrowserFullscreen() {}
-bool get browserFullscreenSupported => false;
 bool get browserInFullscreen => false;
 void addBrowserFullscreenChangeListener(void Function() callback) {}
 void removeBrowserFullscreenChangeListener(void Function() callback) {}
